42c1432e744d2f1311361f2f7c68b515b3a1bb97
Elton Chavez <elton.mct@gmail.com>
2021-08-25 01:42:05 -0300

    carrito done

---
+++ src/components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <th scope="row" colspan="2">Total productos</th>
+    <td>{{totalCantidad}}</td>
+    <td>
+        <button class="btn btn-danger btn-sm" id="vaciar-carrito" @click="resetCarrito">
+            vaciar todo
+        </button>
+    </td>
+    <td class="font-weight-bold">$ <span>{{totalPrecio}}</span></td>
+</template>
+
+<script>
+import { computed } from '@vue/reactivity'
+import { useStore } from 'vuex'
+export default {
+  setup(){
+    const store = useStore()
+    const totalCantidad = computed(() => store.getters.totalCantidad)
+    const totalPrecio = computed(() => store.getters.totalPrecio)
+    const resetCarrito = () => {store.commit('resetCarrito')}
+
+    return {
+      totalCantidad,
+      totalPrecio,
+      resetCarrito
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
